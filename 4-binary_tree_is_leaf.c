e2bfede2ae821419a85fd10126b6b52ad54bd82a
CoderYounes <younesanas2021@gmail.com>
2023-11-29 16:23:59 +0100

    fix some betty style issues

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - check if certain node is leaf
+ * @node: pointer to node
+ * Return: 0 if not leaf , else 1
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
