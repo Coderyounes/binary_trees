536067b2faf1c7e662850fe760e07afef734d6b2
CoderYounes <younesanas2021@gmail.com>
2023-11-29 20:44:25 +0100

    fix some betty style issue

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,12 +1,15 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - calculate tree height
+ * @tree: pointer to tree
+ * Return: the max height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftheight = 0 , rightheight = 0;
-	
+	size_t leftheight = 0, rightheight = 0;
+
 	if (tree == NULL)
 		return (0);
 
