33edd67b1f3c192a50d93425bfef6bba3b490dd3
CoderYounes <younesanas2021@gmail.com>
2023-11-29 23:20:04 +0100

    count the size of a tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - calculate the size of tree
+ * @tree: pointer to tree
+ * Return: 0 if fail , otherwise the size of the tree
+ * Description: this function count nodes recursivly until hit the NULL
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
