e7361eb9716eae8550c72e76bea0f3ba6298712f
CoderYounes <younesanas2021@gmail.com>
2023-11-29 20:42:12 +0100

    calculate the height of the tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftheight = 0 , rightheight = 0;
+	
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		leftheight = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		rightheight = 1 + binary_tree_height(tree->right);
+
+	if (leftheight >= rightheight)
+		return (leftheight);
+	else
+		return (rightheight);
+}
