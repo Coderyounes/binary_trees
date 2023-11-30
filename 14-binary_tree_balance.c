3ffee1b6194eef1fdae513162265c60f81e09e39
CoderYounes <younesanas2021@gmail.com>
2023-11-30 13:21:30 +0100

    second fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,16 +8,45 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int leftheight = 0, rightheight = 0, k = 0;
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	return (0);
+}
+
+
+/**
+ * binary_tree_height - calculate tree height
+ * @tree: pointer to tree
+ * Return: the max height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftheight = 0, rightheight = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
-		leftheight = 1 + binary_tree_balance(tree->left);
-	if (tree->right)
-		rightheight = 1 + binary_tree_balance(tree->right);
-	k = leftheight - rightheight;
+	{
+		leftheight = 1 + binary_tree_height(tree->left);
+	}
+	else
+	{
+		leftheight = 1;
+	}
 
-	return (k);
+	if (tree->right)
+	{
+		rightheight = 1 + binary_tree_height(tree->right);
+	}
+	else
+	{
+		rightheight = 1;
+	}
+
+	if (leftheight >= rightheight)
+		return (leftheight);
+	else
+		return (rightheight);
 }
