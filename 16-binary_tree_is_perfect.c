67c370d936780293fb386643052428984c84da1b
CoderYounes <younesanas2021@gmail.com>
2023-11-30 23:25:19 +0100

    fix from chatgpt

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,48 +1,55 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - check if binary tree is perfect
- * @tree: pointer to tree
- * Return: 0 if false , 1 if true
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ * Return: Height of the tree
  */
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	int left_depth = 0, right_depth = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left_depth = binary_tree_height(tree->left);
-	right_depth = binary_tree_height(tree->right);
-
-	if (left_depth == right_depth)
-		return (1);
-
-	return (0);
-}
-
-
-/**
- * binary_tree_height - calculate the height of each side
- * @tree: pointer to tree
- * Return: the height of a tree
- */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftheight = 0, rightheight = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		leftheight = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		rightheight = 1 + binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (leftheight >= rightheight)
-		return (leftheight);
+	if (left_height > right_height)
+		return (left_height + 1);
 	else
-		return (rightheight);
+		return (right_height + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = binary_tree_height(tree);
+	size_t nodes = binary_tree_size(tree);
+	size_t expected_nodes = (1 << height) - 1;
+
+	return (nodes == expected_nodes);
+}
+
+/**
+ * binary_tree_size - calculate the size of tree
+ * @tree: pointer to tree
+ * Return: 0 if fail , otherwise the size of the tree
+ * Description: this function count nodes recursivly until hit the NULL
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	}
 }
